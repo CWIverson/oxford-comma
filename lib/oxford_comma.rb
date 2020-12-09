cb23b61f483a87ccaa390a30231112efc23766c2
christian iverson <chris.w.iverson@gmail.com>
2020-12-08 16:32:39 -0800

    commit tree 59d97900ea9c50f775a9bf49f78474d5cdfa4a13

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+   middle_array=[]
+    if array.length == 1
+        array.join()
+    elsif array.length == 2
+        array.join(' and ')
+    else
+        middle_array.push('and ' + array[-1])
+        array.pop
+        middle_array.unshift(*array)
+        middle_array.join(', ')
+    end
+    
 end
